0f46c95c25df29a6081f800b131b1bd43b51fd6b
bbyhnu <920237415@qq.com>
2021-09-10 23:30:02 +0800

    add index and bootstrap

---
+++ src/main/java/com/bby/community/Controller/IndexController.java
@@ -0,0 +1,10 @@
+package com.bby.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")//什么都不输入，默认访问/ 根目录
+    public String index(){return "index";}
+}
